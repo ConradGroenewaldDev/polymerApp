21f20da07da5fa6f02f8439680224d729e1f362d
Conrad Groenewald <conradnoah@Conrads-MacBook-Air.local>
2016-03-28 02:07:18 +0100

    added poly structure

--- demo-server/app.js
+++ demo-server/app.js
@@ -1,3 +1,13 @@
 /**
  * Created by conradnoah on 27/03/2016.
  */
+var express  = require("../node_modules/express");
+var app      = express();
+var path     = require("path");
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname+'/../app/index.html'));
+});
+app.use(express.static(path.join(__dirname, '../')));
+var http = require('http').Server(app);
+http.listen(8080);
